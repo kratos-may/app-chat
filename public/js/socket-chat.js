5d1c94d0672e7077860d5271096f2e3bbff1f8cd
kratos-may <ejqr1993.12.30@gmail.com>
2020-06-15 08:56:30 -0400

    listo para produccion

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -1,19 +1,26 @@
 var socket = io();
 
 var params = new URLSearchParams(window.location.search);
-if(!params.has("nombre")|| !params.has("sala")){
-    alert("El nombre y sala son necesarios para poder accesar");
-    window.location = "index.html"
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre y sala son necesarios');
 }
+
 var usuario = {
-    nombre: params.get("nombre"),
-    sala: params.get("sala")
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
 };
+
+
+
 socket.on('connect', function() {
     console.log('Conectado al servidor');
-    socket.emit("entrarChat",usuario,function(resp){
-        console.log("usuarios conectados", resp)
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        renderizarUsuarios(resp);
     });
+
 });
 
 // escuchar
@@ -22,27 +29,21 @@ socket.on('disconnect', function() {
     console.log('Perdimos conexión con el servidor');
 
 });
-
-
-// Enviar información
-/*socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function(resp) {
-    console.log('respuesta server: ', resp);
-});*/
-
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
-    console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje,false);
+    scrollBottom();
 });
-//escuchar cuado un usuario entra y sale de un chat
+
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
 socket.on('listaPersonas', function(personas) {
-    console.log(personas);
+    renderizarUsuarios(personas);
 });
-// mensajes privados
-socket.on("mensajePrivado", function(mensaje){
-    console.log("mensaje privado", mensaje)
-})
+
+// Mensajes privados
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('Mensaje Privado:', mensaje);
+
+});
